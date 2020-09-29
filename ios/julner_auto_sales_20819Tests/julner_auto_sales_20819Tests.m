c9b394be082a997d6860c75135d18c68e8a107dc
Crowdbotics <team@crowdbotics.com>
2020-09-29 13:33:38 +0000

    Initial commit.

---
+++ ios/julner_auto_sales_20819Tests/julner_auto_sales_20819Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface julner_auto_sales_20819 : XCTestCase
+
+@end
+
+@implementation julner_auto_sales_20819
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
