31a69f32dba0291556be074c7796ca3ac5ec0899
Crowdbotics <team@crowdbotics.com>
2020-09-29 13:35:41 +0000

    Update models

--- backend/julner_auto_sales_20819/wsgi.py
+++ backend/julner_auto_sales_20819/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'julner_auto_sales_20819.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "julner_auto_sales_20819.settings")
 
 application = get_wsgi_application()
